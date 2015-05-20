008a1aeda62b9b38469eb971187775587cca548a
Jesenko <jesenko@BitCamps-Mac-mini-31.local>
2015-05-20 21:14:42 +0200

    zadaci

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import Models.MathCal;
+import models.MathCal;
 import play.*;
 import play.data.DynamicForm;
 import play.data.Form;
@@ -16,13 +16,38 @@ public class Application extends Controller {
 	public static Result result() {
 		DynamicForm form = Form.form().bindFromRequest();
 		String expression = form.get("expression");
+		if(expression.equals("")){
+			flash("error", "Your field is empty!");
+			return redirect("/");
+		}
 		return ok(result.render(MathCal.GetIntResult(expression)));
 
 	}
 
 	public static Result stringNum(){
     	DynamicForm form= Form.form().bindFromRequest();
+    	if(form.get("engnum").equals("")){
+			flash("error", "Your field is empty!");
+			return redirect("/");
+		}
     	int number = Integer.parseInt(form.get("engnum"));
+    	
     	return ok(engnumber.render(MathCal.GetStringResult(number)));
     }
+	
+	public static Result wordExpression(){
+		DynamicForm form= Form.form().bindFromRequest();
+		String expression= form.get("engexpression");
+		if(expression.equals("")){
+			flash("error", "Your field is empty!");
+			return redirect("/");
+		}
+		String numberInWords=MathCal.GetStringResult(MathCal.GetIntResult(expression));
+		int number = MathCal.GetIntResult(expression);
+		return ok(wordexpression.render(numberInWords,number));
+	}
+	
+	public static Result calculator(){
+		return ok(calculator.render());
+	}
 }
